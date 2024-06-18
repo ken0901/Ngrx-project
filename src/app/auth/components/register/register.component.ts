ed3eeed1b831a61ec538659d751ac67bbf80ad53
ken0901 <ken5260715@gmail.com>
2024-06-17 22:24:17 -0700

    Register layout - form

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  form = this.fb.nonNullable.group({
+    username: ['',Validators.required],
+    email: ['',Validators.required],
+    password: ['',Validators.required]
+  });
 
+  constructor(private fb: FormBuilder){}
+
+  onSubmit() {
+    console.log('form', this.form.getRawValue());
+  }
 }
