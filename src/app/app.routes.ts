373fd51ea8392e27c22ee25216faa2dfa596be03
ken0901 <ken5260715@gmail.com>
2024-06-17 22:11:14 -0700

    Register component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'register',
+        loadChildren: () => import('../app/auth/auth.routes').then((m) => m.registerRoutes),
+    }
+];
