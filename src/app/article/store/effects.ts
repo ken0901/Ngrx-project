9eb0d2a869cbf7dd504fae20e9745fe1b659ffbf
ken0901 <ken5260715@gmail.com>
2024-07-15 21:56:05 -0700

    Deleting article

--- src/app/article/store/effects.ts
+++ src/app/article/store/effects.ts
@@ -1,10 +1,12 @@
 import { inject } from "@angular/core"
 import { Actions, createEffect, ofType } from "@ngrx/effects"
-import { catchError, map, of, switchMap } from "rxjs"
-import { ArticleService as SharedArticleService } from "../services/article.service"
+import { catchError, map, of, switchMap, tap } from "rxjs"
+import { ArticleService as SharedArticleService } from "../../shared/services/article.service"
 import { ArticleResponseInterface } from "../types/articleResponse.interface"
 import { articleActions } from "./actions"
 import { ArticleInterface } from "../../shared/types/article.interface"
+import { ArticleService } from "../services/article.service"
+import { Router } from "@angular/router"
 
 export const getArticleEffect = createEffect((
     actions$ = inject(Actions),
@@ -23,4 +25,35 @@ export const getArticleEffect = createEffect((
             )
         })
     )
-}, {functional: true})
+}, {functional: true});
+
+export const deleteArticleEffect = createEffect((
+    actions$ = inject(Actions),
+    articleService = inject(ArticleService),
+) => {
+    return actions$.pipe(
+        ofType(articleActions.deleteArticle),
+        switchMap(({slug}) => {
+            return articleService.deleteArticle(slug).pipe(
+                map(() => {
+                    return articleActions.deleteArticleSuccess()
+                }), 
+                catchError(() => {
+                    return of(articleActions.deleteArticleFailure())
+                })
+            )
+        })
+    )
+}, {functional: true});
+
+export const redirectAfterDeleteEffect = createEffect(
+    (actions$ = inject(Actions), router = inject(Router)) => {
+        return actions$.pipe(
+            ofType(articleActions.deleteArticleSuccess),
+            tap(() => {
+                router.navigateByUrl('/')
+            })
+        )
+    },
+    {functional: true, dispatch: false}
+)
--- src/app/shared/services/article.service.ts
+++ src/app/shared/services/article.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
+import { ArticleInterface } from '../types/article.interface';
 import { environment } from '../../../environments/environment';
+import { ArticleResponseInterface } from '../../article/types/articleResponse.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +12,8 @@ export class ArticleService {
 
   constructor(private http: HttpClient) { }
 
-  deleteArticle(slug: string): Observable<{}> {
+  getArticle(slug: string): Observable<ArticleInterface> {
     const fullUrl = `${environment.apiUrl}/articles/${slug}`
-    return this.http.delete(fullUrl);
+    return this.http.get<ArticleResponseInterface>(fullUrl).pipe(map((response) => response.article));
   }
 }
