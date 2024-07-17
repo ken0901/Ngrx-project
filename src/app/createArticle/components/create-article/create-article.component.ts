b099b5fcc3e2db90ca2739cecb95ead129d70e81
ken0901 <ken5260715@gmail.com>
2024-07-16 21:45:45 -0700

    Creating article form

--- src/app/createArticle/components/create-article/create-article.component.ts
+++ src/app/createArticle/components/create-article/create-article.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { ArticleFormValuesInterface } from '../../../shared/components/article-form/types/articleFormValues.interface';
+import { ArticleFormComponent } from '../../../shared/components/article-form/article-form.component';
 
 @Component({
   selector: 'app-create-article',
   standalone: true,
-  imports: [],
+  imports: [ArticleFormComponent],
   templateUrl: './create-article.component.html',
   styleUrl: './create-article.component.css'
 })
 export class CreateArticleComponent {
+  initialValues = {
+    title: '',
+    description: '',
+    body: '',
+    tagList: []
+  }
 
+  onSubmit(articleFormValues: ArticleFormValuesInterface): void {
+    console.log('onSubmit in create article', articleFormValues);
+  }
 }
--- src/app/shared/components/article-form/article-form.component.ts
+++ src/app/shared/components/article-form/article-form.component.ts
@@ -1,18 +1,55 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ArticleFormValuesInterface } from './types/articleFormValues.interface';
 import { BackendErrorsInterface } from '../../types/backendErrors.interface';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { BackendErrorMessagesComponent } from "../backend-error-messages/backend-error-messages.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-article-form',
   standalone: true,
-  imports: [],
+  imports: [BackendErrorMessagesComponent, ReactiveFormsModule, CommonModule],
   templateUrl: './article-form.component.html',
   styleUrl: './article-form.component.css'
 })
-export class ArticleFormComponent {
+export class ArticleFormComponent implements OnInit{
   @Input() initialValues?: ArticleFormValuesInterface;
   @Input() isSubmitted: boolean = false;
   @Input() errors: BackendErrorsInterface | null = null;
 
   @Output() articleSubmit = new EventEmitter<ArticleFormValuesInterface>();
+
+  form = this.fb.nonNullable.group({
+    title: '',
+    description: '',
+    body: '',
+    tagList: '',
+  });
+
+  constructor(private fb: FormBuilder){}
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    if(!this.initialValues) {
+      throw new Error('Inputs are not provided');
+    }
+    this.form.patchValue({
+      title: this.initialValues.title,
+      description: this.initialValues.description,
+      body: this.initialValues.body,
+      tagList: this.initialValues.tagList.join(' '),
+    });
+  }
+
+  onSubmit(): void {
+    const formValue = this.form.getRawValue();
+    const articleFormValues: ArticleFormValuesInterface = {
+      ...formValue,
+      tagList: formValue.tagList.split(' '),
+    }
+    this.articleSubmit.emit(articleFormValues);
+  }
 }
